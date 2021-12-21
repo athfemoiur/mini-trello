de99243a43ff50e9dc2811d9d3192e5c92d7ff92
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-12-21 09:30:54 +0330

    add sentry

--- src/trello/envs/development.py
+++ src/trello/envs/development.py
@@ -1,3 +1,51 @@
 from .common import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = True
+
+# logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/tmp/debug.log'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file']
+        }
+    }
+}
+
+sentry_sdk.init(
+    dsn="https://a85edd5f51654f20b8fd95efe3a8ceff@o1095889.ingest.sentry.io/6115869",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- src/trello/urls.py
+++ src/trello/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('secret-admin/', admin.site.urls),
-    path('api/users/', include('users.urls', namespace='users'))
+    path('api/users/', include('users.urls', namespace='users')),
+    path('sentry-debug/', trigger_error),
 ]
